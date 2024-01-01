bd884ab8c3d49d6e73c366e5911d1967ea111440
21120430 <ngotuandat.lop9a@gmail.com>
2024-01-01 17:08:22 +0700

    Review all

--- src/app.js
+++ src/app.js
@@ -16,8 +16,17 @@ app.use(express.static('public'));
 
 app.get('/home', (req, res) => {
     res.render('home', {
-        title: 'Home Page'
+        title: 'Home Page',
+        text: 'Hello World',
+        oke: false,
+        arr: [1, 2, 3, 4, 5]
     });
+});
+
+app.get('/bills', (req, res) => {
+    res.render('bills', {
+        title: 'Bills'
+    })
 })
 
 app.listen(port, () => {

69b8484f11620bc5a0f389dcfd5d651a5347cab1
21120442 <duydangtr@gmail.com>
2024-01-03 21:32:52 +0700

    Update db, add product model + controller

---
+++ src/model/product.m.js
@@ -0,0 +1,31 @@
+const db = require('../utils/db');
+const tbName = "SanPham";
+
+module.exports = class Product {
+    constructor(MaSP, Ten, DonGia, SoLuongTon, Anh, MaLoai) {
+        this.MaSP = MaSP;
+        this.Ten = Ten;
+        this.DonGia = DonGia;
+        this.SoLuongTon = SoLuongTon;
+        this.Anh = Anh;
+        this.MaLoai = MaLoai;
+    }
+    static async insert(product) {
+        return db.insert(tbName, product);
+    }
+    static async getProduct(id) {
+        return db.select(tbName, "MaSP", id);
+    }
+    static async getAll() {
+        return db.selectAll(tbName);
+    }
+    static async getAllBy(orderBy, isDesc) {
+        return db.selectAllBy(tbName, orderBy, isDesc);
+    }
+    static async getBy(type) {
+        return db.joinTB(tbName, "Loai", "MaLoai", "TenLoai", type);
+    }
+    static async getBy(type, orderBy, isDesc) {
+        return db.joinTB(tbName, "Loai", "MaLoai", "MaLoai", "TenLoai", type, orderBy, isDesc, null);
+    }
+}
--- src/utils/db.js
+++ src/utils/db.js
@@ -32,16 +32,32 @@ module.exports = {
         }
     },
 
-    delete: async (tbName, id) => {
+    insert: async (tbName, entity) => {
+        try {
+            const query = pgp.helpers.insert(entity, null, tbName);
+            const result = await db.one(query + ' returning *');
+            if (result) {
+                console.log(`1 row inserted to ${tbName}.`);
+                return result;
+            } else {
+                console.log('No data returned from the query.');
+                return null;
+            }
+        } catch (error) {
+            console.log('Insert error: ', error);
+        }
+    },
+
+    delete: async (tbName, col, val) => {
         try {
             const result = await db.result(
-                `DELETE FROM "${tbName}" WHERE id = $1 RETURNING *`, [id]
+                `DELETE FROM "${tbName}" WHERE ${col} = $1 RETURNING *`, [val]
             );
             if (result.rowCount > 0) {
-                console.log(`Deleted row with id ${id}`);
+                console.log(`Deleted row with value ${val} in colmn ${col}`);
             }
             else {
-                console.log(`No rows deleted. Row with id ${id} not found`);
+                console.log(`No rows deleted. Row not found`);
             }
         } catch (error) {
             console.log('Delete error: ', error);
@@ -51,39 +67,61 @@ module.exports = {
     selectAll: async (tbName) => {
         let dbcn = null;
         try {
-            dbcn = await db.connect();
             const data = await dbcn.any(`SELECT * FROM "${tbName}"`);
             return data;
         }
         catch (error) {
             console.log('Select all error: ', error);
         }
-        finally {
-            dbcn.done();
+    },
+
+    selectAllBy: async (tbName, colOrder, isDesc) => {
+        let dbcn = null;
+        try {
+            const query = `
+            SELECT * FROM "${tbName}"
+            `;
+            if (colOrder) {
+                if (isDesc) {
+                    query += `ORDER BY ${colOrder} DESC `;
+                }
+                else query += `ORDER BY ${colOrder} ASC `;
+            }
+            const data = await dbcn.any(query);
+            return data;
+        }
+        catch (error) {
+            console.log('Select all error: ', error);
         }
     },
 
-    selectTopByProp: async (tbName, prop, limit) => {
+    selectTopByCol: async (tbName, col, limit, isDesc) => {
         try {
             const query = `
             SELECT * 
             FROM ${tbName}
-            WHERE ${prop} IS NOT NULL
-            ORDER BY ${prop} DESC
-            LIMIT ${limit}
-        `;
+            WHERE ${col} IS NOT NULL 
+            `;
+            if (isDesc) {
+                query += `ORDER BY ${col} DESC `;
+            }
+            else query += `ORDER BY ${col} ASC `;
+
+            if (limit) {
+                query += `LIMIT ${limit} `;
+            }
             return db.manyOrNone(query);
         } catch (error) {
             console.log('Select top by property error: ', error);
         }
     },
 
-    select: async (tbName, prop, condition) => {
+    select: async (tbName, col, val) => {
         try {
             const query = `
                 SELECT *
                 FROM "${tbName}"
-                WHERE "${prop}" = '${condition}'
+                WHERE "${col}" = '${val}'
             `;
             return db.oneOrNone(query);
         } catch (error) {
@@ -104,15 +142,39 @@ module.exports = {
         }
     },
 
-    joinTB: async (tb1, tb2, cond, prop, condition) => {
+    joinTB: async (tb1, tb2, col1, col2, val) => {
         try {
             const query = `
                 SELECT *
                 FROM "${tb1}"
-                JOIN "${tb2}" ON "${tb1}"."${cond}" = "${tb2}"."${cond}"
-                WHERE "${prop}" = $1
+                JOIN "${tb2}" ON "${tb1}"."${col1}" = "${tb2}"."${col1}"
+                WHERE "${col2}" = $1
             `;
-            return db.manyOrNone(query, [condition]);
+            return db.manyOrNone(query, [val]);
+        } catch (error) {
+            console.log("Join table error: ", error);
+        }
+    },
+
+    joinTB: async (tb1, tb2, col1, col2, colWhere, val, colOrder, isDesc, limit) => {
+        try {
+            const query = `
+                SELECT *
+                FROM "${tb1}"
+                JOIN "${tb2}" ON "${tb1}"."${col1}" = "${tb2}"."${col2}"
+                WHERE "${colWhere}" = $1
+            `;
+            if (colOrder) {
+                if (isDesc) {
+                    query += `ORDER BY ${colOrder} DESC `;
+                }
+                else query += `ORDER BY ${colOrder} ASC `;
+            }
+
+            if (limit) {
+                query += `LIMIT ${limit} `;
+            }
+            return db.manyOrNone(query, [val]);
         } catch (error) {
             console.log("Join table error: ", error);
         }

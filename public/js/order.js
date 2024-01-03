ac8eb0569d136dcb3bfc382c6936ca373b78e1b6
21120495 <phtmyle@gmail.com>
2024-01-03 13:50:33 +0700

    fix order

--- public/js/order.js
+++ public/js/order.js
@@ -1,15 +1,15 @@
 function checkOrderConditions() {
     const hasItems = $(".order-content .item").length > 0;
-    const paymentOptionChosen = $(".btn-check:checked").length > 0;
+    const paymentOptionChosen = $(".payment-btn:checked").length > 0;
     $(".order-btn").prop("disabled", !(hasItems && paymentOptionChosen));
 }
 checkOrderConditions();
-$(".bx-trash").on("click", (event) => {
+$(".delete-order").on("click", (event) => {
     $(event.target).closest(".item").remove();
     checkOrderConditions();
 });
 
-$(".bt-cnheck").on("change", () => {
+$(".payment-btn").on("change", () => {
     checkOrderConditions();
 });
 
